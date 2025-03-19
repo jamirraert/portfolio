47bc9fe498995160b62d4c63ab1f5a2916290562
jamirraert0122 <c.baquiano22@gmail.com>
2025-03-19 11:13:45 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <RouterLink :to="{name: 'about'}">Go to About</RouterLink>
   </nav>
   <main>
+    <h1 class="text-2xl font-uderline bg-red-400">test</h1>
     <RouterView />
   </main>
 </template>
